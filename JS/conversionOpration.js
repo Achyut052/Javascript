18510366a07ed2ab02b4837d05309a47db0e009d
ACHYUT VAGHASIYA <118907907+Achyut052@users.noreply.github.com>
2024-05-12 18:08:21 +0000

    conversion of opration

--- JS/conversionOpration.js
+++ JS/conversionOpration.js
@@ -3,4 +3,30 @@ let score = 33
 console.log(typeof score);
 
 let valueInNumber = Number(score)
+console.log(valueInNumber);
 console.log(typeof valueInNumber);
+
+/*
+conversion of number
+// 33 => 33
+// "33abc" => NaN
+// true=>1; false => 0
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+/*
+conversion of Boolean
+1 =>true; 0 => false
+""=> false; "abc" => true 
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
