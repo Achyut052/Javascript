937f0c31eef86aa4235527263c86018bfeb0ab0b
ACHYUT VAGHASIYA <118907907+Achyut052@users.noreply.github.com>
2024-05-12 18:26:43 +0000

    opration

--- JS/conversionOpration.js
+++ JS/conversionOpration.js
@@ -30,3 +30,33 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+/********* OPRATIONS **********/
+
+let value =3
+let negativeValue = -value
+console.log(negativeValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%3);
+
+let str1 = "abc"
+let str2 = "def"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log(+true); //1
+console.log(+""); //0
+
+let count = 100
+++count;
+console.log(count);
